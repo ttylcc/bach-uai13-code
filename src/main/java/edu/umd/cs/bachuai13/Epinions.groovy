b2c9ddb73843b6300c124d331d3c55b350a86f56
ttylcc <ttylacm@126.com>
2017-03-09 00:27:05 -0800

    change folds to 2

--- src/main/java/edu/umd/cs/bachuai13/Epinions.groovy
+++ src/main/java/edu/umd/cs/bachuai13/Epinions.groovy
@@ -157,7 +157,7 @@ inserter = data.getInserter(trusts, fullTrusts)
 InserterUtils.loadDelimitedDataTruth(inserter, dataPath + "trusts.txt")
 
 // number of folds
-folds = 8
+folds = 2
 
 List<Partition> trustsPartitions = new ArrayList<Partition>(folds)
 List<Partition> knowsPartitions = new ArrayList<Partition>(folds)
