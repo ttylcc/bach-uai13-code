dca5bbb8435d30da27ed99989175513521dfdd26
ttylcc <ttylacm@126.com>
2017-03-09 10:58:10 -0800

    sh for latentTest

--- runSocialTrustPrediction.sh
+++ runSocialTrustPrediction.sh
@@ -6,12 +6,13 @@ mvn dependency:build-classpath -Dmdep.outputFile=classpath.out > /dev/null
 mkdir output > /dev/null
 
 echo "Running HL-MRF-Q on Epinions..."
-java -Xmx1g -cp ./target/classes:`cat classpath.out` edu.umd.cs.bachuai13.Epinions quad > output/epinions.hlmrf-q.out
-echo "Running HL-MRF-L on Epinions..."
-java -Xmx1g -cp ./target/classes:`cat classpath.out` edu.umd.cs.bachuai13.Epinions linear > output/epinions.hlmrf-l.out
-echo "Running MRF on Epinions..."
-java -Xmx1g -cp ./target/classes:`cat classpath.out` edu.umd.cs.bachuai13.Epinions bool > output/epinions.mrf.out
+java -Xmx1g -cp ./target/classes:`cat classpath.out` edu.umd.cs.bachuai13.EpinionsLatentTest quad > output/epinionsLatentTest.hlmrf-q.out
+# java -Xmx1g -cp ./target/classes:`cat classpath.out` edu.umd.cs.bachuai13.Epinions quad > output/epinions.hlmrf-q.out
+# echo "Running HL-MRF-L on Epinions..."
+# java -Xmx1g -cp ./target/classes:`cat classpath.out` edu.umd.cs.bachuai13.Epinions linear > output/epinions.hlmrf-l.out
+# echo "Running MRF on Epinions..."
+# java -Xmx1g -cp ./target/classes:`cat classpath.out` edu.umd.cs.bachuai13.Epinions bool > output/epinions.mrf.out
 
-echo "Processing results..."
-cd src/main/matlab
-matlab -nodesktop -nosplash -r parse_epinions
+# echo "Processing results..."
+# cd src/main/matlab
+# matlab -nodesktop -nosplash -r parse_epinions
