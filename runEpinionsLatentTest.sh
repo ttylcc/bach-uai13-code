531851fe9776f3d9c627bdd3cc06626b9f02cb6c
ttylcc <ttylacm@126.com>
2017-03-11 21:30:49 -0800

    change name

--- runEpinionsLatentTest.sh
+++ runEpinionsLatentTest.sh
@@ -5,13 +5,13 @@ mvn compile > /dev/null
 mvn dependency:build-classpath -Dmdep.outputFile=classpath.out > /dev/null
 mkdir output > /dev/null
 
-echo "Running HL-MRF-Q on Epinions..."
-java -Xmx1g -cp ./target/classes:`cat classpath.out` edu.umd.cs.bachuai13.EpinionsLatentTest quad > output/epinionsLatentTest.hlmrf-q.out
-echo "Running HL-MRF-L on Epinions..."
-java -Xmx1g -cp ./target/classes:`cat classpath.out` edu.umd.cs.bachuai13.EpinionsLatentTest linear > output/epinionsLatentTest.hlmrf-l.out
-echo "Running MRF on Epinions..."
-java -Xmx1g -cp ./target/classes:`cat classpath.out` edu.umd.cs.bachuai13.EpinionsLatentTest bool > output/epinionsLatentTest.mrf.out
+echo "Running HL-MRF-Q on wiki..."
+java -Xmx1g -cp ./target/classes:`cat classpath.out` edu.umd.cs.bachuai13.WikiLatentTest quad > output/WikiLatentTest.hlmrf-q.out
+echo "Running HL-MRF-L on wiki..."
+java -Xmx1g -cp ./target/classes:`cat classpath.out` edu.umd.cs.bachuai13.WikiLatentTest linear > output/WikiLatentTest.hlmrf-l.out
+echo "Running MRF on wiki..."
+java -Xmx1g -cp ./target/classes:`cat classpath.out` edu.umd.cs.bachuai13.WikiLatentTest bool > output/WikiLatentTest.mrf.out
 
 echo "Processing results..."
 # cd src/main/matlab
-# matlab -nodesktop -nosplash -r parse_epinions
+# matlab -nodesktop -nosplash -r parse_wiki
--- runEpinionsTest.sh
+++ runEpinionsTest.sh
@@ -5,13 +5,13 @@ mvn compile > /dev/null
 mvn dependency:build-classpath -Dmdep.outputFile=classpath.out > /dev/null
 mkdir output > /dev/null
 
-echo "Running HL-MRF-Q on Epinions..."
-java -Xmx1g -cp ./target/classes:`cat classpath.out` edu.umd.cs.bachuai13.Epinions quad > output/epinions.hlmrf-q.out
-# echo "Running HL-MRF-L on Epinions..."
-# java -Xmx1g -cp ./target/classes:`cat classpath.out` edu.umd.cs.bachuai13.Epinions linear > output/epinions.hlmrf-l.out
-# echo "Running MRF on Epinions..."
-# java -Xmx1g -cp ./target/classes:`cat classpath.out` edu.umd.cs.bachuai13.Epinions bool > output/epinions.mrf.out
+echo "Running HL-MRF-Q on Wiki..."
+java -Xmx1g -cp ./target/classes:`cat classpath.out` edu.umd.cs.bachuai13.Wiki quad > output/Wiki.hlmrf-q.out
+# echo "Running HL-MRF-L on wiki..."
+# java -Xmx1g -cp ./target/classes:`cat classpath.out` edu.umd.cs.bachuai13.Wiki linear > output/Wiki.hlmrf-l.out
+# echo "Running MRF on wiki..."
+# java -Xmx1g -cp ./target/classes:`cat classpath.out` edu.umd.cs.bachuai13.Wiki bool > output/Wiki.mrf.out
 
 echo "Processing results..."
 # cd src/main/matlab
-# matlab -nodesktop -nosplash -r parse_epinions
+# matlab -nodesktop -nosplash -r parse_wiki
